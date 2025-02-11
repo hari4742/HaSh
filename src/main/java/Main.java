433531ded676be746b8b29265a18cd6080163cfd
HariKiran <harikiran.kante@gmail.com>
2025-02-11 11:49:20 +0530

    type cmd with PATH

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,6 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        Scanner scanner = new Scanner(System.in);
-        String input;
-
-        do {
-            System.out.print("$ ");
-            input = scanner.nextLine();
-            String[] instructions = input.split(" ");
-            String cmd = instructions[0];
-            String[] builtins = { "exit", "echo", "type" };
-
-            switch (cmd) {
-                case "exit":
-                    System.exit(0);
-                    scanner.close();
-                    break;
-
-                case "echo":
-                    for (int i = 1; i < instructions.length; i++) {
-                        System.out.printf("%s ", instructions[i]);
-                    }
-                    System.out.println();
-                    break;
-
-                case "type":
-                    if (instructions.length < 2) {
-                        System.out.println("usage type <command>");
-                        break;
-                    }
-                    String cmdName = instructions[1];
-                    if (Arrays.asList(builtins).contains(cmdName)) {
-                        System.out.printf("%s is a shell builtin\n", cmdName);
-                    } else {
-                        System.out.printf("%s: not found\n", cmdName);
-                    }
-                    break;
-
-                default:
-                    System.out.println(input + ": command not found");
-                    break;
-            }
-
-        } while (true);
-
+        Shell shell = new Shell();
+        shell.repl();
     }
 }
---
+++ src/main/java/Shell.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+
+public class Shell {
+    String[] instructions;
+
+    public void repl() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            System.out.print("$ ");
+            input = scanner.nextLine();
+            instructions = input.split(" ");
+            String cmd = instructions[0];
+
+            switch (cmd) {
+                case "exit":
+                    exit();
+                    scanner.close();
+                    break;
+
+                case "echo":
+                    echo();
+                    break;
+
+                case "type":
+                    type();
+                    break;
+
+                default:
+                    System.out.println(input + ": command not found");
+                    break;
+            }
+
+        } while (true);
+    }
+
+    void exit() {
+        System.exit(0);
+    }
+
+    void echo() {
+        for (int i = 1; i < instructions.length; i++) {
+            System.out.printf("%s ", instructions[i]);
+        }
+        System.out.println();
+    }
+
+    void type() {
+        if (instructions.length < 2) {
+            System.out.println("usage type <command>");
+            return;
+        }
+        String cmdName = instructions[1];
+        if (checkBuiltins(cmdName)) {
+            System.out.printf("%s is a shell builtin\n", cmdName);
+            return;
+        }
+        String path = getPath(cmdName);
+        if (path != null) {
+            System.out.printf("%s is %s\n", cmdName, path);
+            return;
+        }
+
+        System.out.printf("%s: not found\n", cmdName);
+
+    }
+
+    private boolean checkBuiltins(String cmdName) {
+        String[] builtins = { "exit", "echo", "type" };
+        return Arrays.asList(builtins).contains(cmdName);
+    }
+
+    private String getPath(String cmdName) {
+        String PATH = System.getenv("PATH");
+        String pathSep = getPathSep();
+        String[] paths = PATH.split(pathSep);
+
+        for (String path : paths) {
+            File folder = new File(path);
+            if (!folder.exists() || !folder.isDirectory())
+                continue;
+
+            File[] files = folder.listFiles(File::isFile);
+            if (files == null)
+                continue;
+
+            for (File file : files) {
+                System.out.println(file);
+                String fileName = file.getName();
+
+                if (fileName.equals(cmdName))
+                    return file.getPath();
+            }
+
+        }
+        return null;
+    }
+
+    private String getPathSep() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win"))
+            return ";";
+
+        return ":";
+    }
+
+}
